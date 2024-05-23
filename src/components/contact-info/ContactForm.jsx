48def5aec562c351297438586f10ba4e08f24c30
MMonikaFirst <m.marciniakfirst@outlook.com>
2024-05-23 12:05:34 +0200

    testing Monika

--- src/components/contact-info/ContactForm.jsx
+++ src/components/contact-info/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./ContactForm.css"
+import "./ContactForm.css";
 
 function ContactForm() {
   const [name, setName] = useState("");
@@ -9,11 +9,10 @@ function ContactForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
     // console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
-    const contactName = 'Contact Form Submission from ${name}'
-    const body = 'Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}';
-    window.location.href = 'mailto:recipient@example.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}'
-
-
+    const contactName = "Contact Form Submission from ${name}";
+    const body = "Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}";
+    window.location.href =
+      "mailto:recipient@example.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}";
   };
 
   return (
@@ -27,6 +26,7 @@ function ContactForm() {
         />
       </label>
       <br />
+      <div>Habababa ciastko kawa</div>
       <label>
         Email:
         <input
