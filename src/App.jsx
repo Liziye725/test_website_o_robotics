86fe175de5fa4a0e70ddcc0483739fa2301e5493
MMonikaFirst <m.marciniakfirst@outlook.com>
2024-05-27 14:50:42 +0200

    Contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AboutPage from './components/contact-info/AboutPage'
-import ContactForm from './components/contact-info/ContactForm'
+import React from "react";
+import { useState } from "react";
+
+import "./App.css";
+
+import ContactForm from "./components/contact-info/ContactForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <AboutPage/>
       <div>
-        <ContactForm/>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        {/* <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-      {/* </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+        {/* Style it later */}
+        <h1>Contact Us</h1>
+        <ContactForm />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
