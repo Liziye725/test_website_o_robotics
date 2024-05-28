e1e0588f387894e25a3cc9bfe565cf21461812b5
Liziye725 <liziye725@gmail.com>
2024-05-28 17:54:35 +0200

    Add scenario picture to home page

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom'
 import overview1 from '../../assets/home/FullAssembly3.png'
 import overview2 from '../../assets/home/FullAssembly4.png'
 
+import scenarioWeed from '../../assets/home/scenario-weed-big.png'
+import scenarioGarbage from '../../assets/home/scenario-garbage-big.jpg'
+import scenarioSnow from '../../assets/home/scenario-snow-big.png'
+
 export default function HomePage() {
     return (
         <>
@@ -20,7 +24,35 @@ export default function HomePage() {
                     </p>
                 </div>
             </div>
+        </section>
+        <section>
+            <div className='product-view'>
+                <div className='scenario-picture'>
+                    <Link to={'/product1'} className='home-scenario'>
+                    <img src={scenarioWeed} alt="Scenario 1"  style={{ maxWidth: '1000px'}} />
+                    <p>
+                        Weed your yard
+                    </p>
+                    </Link>
+                </div>
+                <div className='scenario-picture'>
+                    <Link to={'/product2'} className='home-scenario'>
+                    <img src={scenarioGarbage} alt="Scenario 2"  style={{ maxWidth: '1000px'}} />
+                    <p>
+                        Clean your garden
+                    </p>
+                    </Link>
+                </div>
+                <div className='scenario-picture'>
+                    <Link to={'/product3'} className='home-scenario'>
+                    <img src={scenarioSnow} alt="Scenario 3"  style={{ maxWidth: '1000px'}} />
+                    <p>
+                        Remove the snow
+                    </p>
+                    </Link>
+                </div>
 
+            </div>
         </section>
         </>
     )
