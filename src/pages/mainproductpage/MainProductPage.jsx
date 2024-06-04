a7448bb26e783cdc3f3618abe2a473dd4722ae70
mariiaipatova <mary.ipatova.dk@gmail.com>
2024-06-04 17:53:10 +0200

    Product pages img changed

--- src/pages/mainproductpage/MainProductPage.jsx
+++ src/pages/mainproductpage/MainProductPage.jsx
@@ -12,19 +12,19 @@ const MainProductPage = () => {
       <h2>Our robots can work in many different ways</h2>
       <div>
         <div>
-          <img src={GarbageScenarioSmall} alt="Garbage" style={{ maxWidth: '300px', maxHeight: '150px' }} />
+          <img src={WeedScenarioSmall} alt="Garbage" style={{ maxWidth: '300px', maxHeight: '150px' }} />
           <button>
             <Link to="/product1">Scenario 1</Link>
           </button>
         </div>
         <div>
-          <img src={SnowScenarioSmall} alt="Snow" style={{ maxWidth: '300px', maxHeight: '150px' }} />
+          <img src={GarbageScenarioSmall} alt="Snow" style={{ maxWidth: '300px', maxHeight: '150px' }} />
           <button>
             <Link to="/product2">Scenario 2</Link>
           </button>
         </div>
         <div>
-          <img src={WeedScenarioSmall} alt="Weed" style={{ maxWidth: '300px', maxHeight: '150px' }} />
+          <img src={SnowScenarioSmall} alt="Weed" style={{ maxWidth: '300px', maxHeight: '150px' }} />
           <button>
             <Link to="/product3">Scenario 3</Link>
           </button>
