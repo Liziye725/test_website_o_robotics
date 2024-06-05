86f0e49452ac80188c00b7ed5b3a1f6056414757
mariiaipatova <mary.ipatova.dk@gmail.com>
2024-06-05 19:00:33 +0200

    product and contact pages menu problem fixed

--- src/pages/productpage-3/ProductPage3.jsx
+++ src/pages/productpage-3/ProductPage3.jsx
@@ -7,7 +7,7 @@ const ProductPage3 = () => {
   return (
     <div>
       <h1>Scenario 3: Snow Removal</h1>
-      <img src={SnowScenarioBig} alt="Product 3" style={{ maxWidth: '300px', maxHeight: '150px' }} />
+      <img src={SnowScenarioBig} alt="Product 3" style={{ maxWidth: '600px' }} />
       <p>Our robot excels in snow removal, making winter maintenance more efficient and less labor-intensive. It clears sidewalks, driveways, and parking lots, ensuring safe and accessible paths. With its robust design and effective snow-handling capabilities, this robot becomes an indispensable tool during the winter months, reducing the effort and time traditionally required for snow clearance.</p>
       <button><Link to="/mainproductpage">Back to Products</Link></button>
     </div>
