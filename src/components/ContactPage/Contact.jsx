4201115919a0db834371a7822150cbe0505d23aa
MMonikaFirst <m.marciniakfirst@outlook.com>
2024-06-14 13:30:01 +0200

    contact up to date

--- src/components/ContactPage/Contact.jsx
+++ src/components/ContactPage/Contact.jsx
@@ -2,7 +2,6 @@ import React from "react";
 // import { CiMail } from "react-icons/ci";
 // import { FaFacebook } from "react-icons/fa";
 // import { GoogleMap, Marker } from "@react-google-maps/api";
-
 const Footer = () => {
   return (
     <footer className="footer">
@@ -32,44 +31,12 @@ const Footer = () => {
             style={{ border: "1px solid #ccc" }}
           ></iframe>
         </div>
-
         <div className="col-md-4">
           <p>&copy; 2024 O-Robotics</p>
           <p>CVR-nr 43505718</p>
         </div>
-    <>
-      <h2>Contact O-Robotics</h2>
-      <div className="contact-container">
-        {/* <p>Get in touch with us!</p> Maybe a paragraph with something? */}
-        <ul>
-          <li>
-            <i className="fas fa-map-marker-alt" />
-            <span>Find us at Ecopark:</span>
-            <br />
-            <br />
-            <address> Bautavej 1A, 8210 Aarhus</address>
-            <br />
-          </li>
-          <li>
-            <i className="fas fa-phone" />
-            <span>Phone:</span>
-            {/* which phone number should we add?  */}
-            <a href="tel:123-456-7890">123-456-7890</a>
-          </li>
-          <li>
-            <i className="fas fa-envelope" />
-            <span>Email:</span>
-            <a href="mailto:hello@o-robotics.com">hello@o-robotics.com</a>
-          </li>
-          <li>
-            <i className="fas fa-globe" />
-            <span>Website:</span>
-            <a href="https://www.O-Robotics.com/">https://www.O-Robotics.com</a>
-          </li>
-        </ul>
       </div>
     </footer>
   );
 };
-
 export default Footer;
